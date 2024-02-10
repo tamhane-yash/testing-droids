a0e8d9014650b1f5e5d0918b354652e7e1c16fb7
yash <tamhane.yash407@gmail.com>
2024-02-10 04:33:23 -0800

    Basic Authentications

--- app/src/main/java/com/example/biometricauthentication/MainActivity.kt
+++ app/src/main/java/com/example/biometricauthentication/MainActivity.kt
@@ -1,24 +1,119 @@
 package com.example.biometricauthentication
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import android.provider.Settings
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentActivity
 import com.example.biometricauthentication.ui.theme.BiometricAuthenticationTheme
+import com.example.biometricauthentication.ui.theme.checkExistence
+import com.example.biometricauthentication.ui.theme.sdkInt
+import com.example.biometricauthentication.ui.theme.showToast
 
-class MainActivity : ComponentActivity() {
+val LocalActivity = compositionLocalOf<FragmentActivity> { error("error") }
+
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BiometricAuthenticationTheme {
-
+                CompositionLocalProvider(LocalActivity provides this) {
+                    BiometricAuth()
+                }
             }
         }
     }
+}
+
+@Composable
+fun BiometricAuth() {
+    val context = LocalContext.current
+    val activity = LocalActivity.current
+    val resultCode = remember { mutableStateOf(Int.MIN_VALUE) }
+    val executors = ContextCompat.getMainExecutor(context)
+    val launcherIntent =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.StartActivityForResult(),
+            onResult = { result ->
+                when (result.resultCode) {
+                    1 -> {
+                        context.showToast("Enrollment Done !!")
+                        resultCode.value = 1
+                    }
+
+                    2 -> {
+                        context.showToast("Rejected")
+                    }
+
+                    else -> {
+                        context.showToast("User Cancel the Biometric")
+                    }
+                }
+
+            })
+    val biometricManager = BiometricManager.from(context)
+    
+    LaunchedEffect(key1 = resultCode.value, block = {
+        biometricManager.checkExistence(onSuccess = {
+            val biometricPromptInfo = BiometricPrompt.PromptInfo
+                .Builder()
+                .setTitle("Authenticate")
+                .setSubtitle("Authenticate Subtitle")
+                .setNegativeButtonText("Cancel")
+                .setAllowedAuthenticators(it)
+                .build()
+
+            val biometricPrompt = BiometricPrompt(
+                activity,
+                executors,
+                object : BiometricPrompt.AuthenticationCallback() {
+                    override fun onAuthenticationFailed() {
+                        super.onAuthenticationFailed()
+                        context.showToast("Failed")
+                    }
+
+                    override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                        super.onAuthenticationSucceeded(result)
+                        context.showToast("success")
+                    }
+
+                    override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                        super.onAuthenticationError(errorCode, errString)
+                        context.showToast("error")
+                    }
+
+                })
+            biometricPrompt.authenticate(biometricPromptInfo)
+
+        },
+            openSettings = {
+                sdkInt(aboveVersion9 = {
+                    val intent = Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
+                        putExtra(
+                            Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+                            BiometricManager.Authenticators.BIOMETRIC_STRONG
+                        )
+                    }
+                    launcherIntent.launch(intent)
+                }, belowVersion10 = {
+                    val intent = Intent(Settings.ACTION_SECURITY_SETTINGS)
+                    activity.startActivity(intent)
+                })
+            },
+            onError = {
+                context.showToast(it)
+            })
+    })
+
 }
---
+++ app/src/main/java/com/example/biometricauthentication/ui/theme/BiometricUtils.kt
@@ -0,0 +1,55 @@
+package com.example.biometricauthentication.ui.theme
+
+import android.os.Build
+import androidx.biometric.BiometricManager
+
+inline fun authenticators(aboveVersion9: () -> Int, belowVersion9: () -> Int): Int {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        aboveVersion9.invoke()
+    } else {
+        belowVersion9.invoke()
+    }
+}
+
+fun BiometricManager.checkExistence(
+    onSuccess: (Int) -> Unit,
+    onError: (String) -> Unit,
+    openSettings: () -> Unit
+) {
+    val authenticators = authenticators(aboveVersion9 = {
+        BiometricManager.Authenticators.BIOMETRIC_STRONG
+    }, belowVersion9 = {
+        BiometricManager.Authenticators.BIOMETRIC_WEAK
+    })
+
+    when (canAuthenticate(authenticators)) {
+        BiometricManager.BIOMETRIC_SUCCESS -> {
+           onSuccess.invoke(authenticators)
+        }
+
+        BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> {
+            onError.invoke("BIOMETRIC_ERROR_HW_UNAVAILABLE")
+        }
+
+        BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+            openSettings.invoke()
+        }
+
+        BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> {
+            onError.invoke("BIOMETRIC_ERROR_NO_HARDWARE")
+        }
+
+        BiometricManager.BIOMETRIC_ERROR_SECURITY_UPDATE_REQUIRED -> {
+            onError.invoke("BIOMETRIC_ERROR_SECURITY_UPDATE_REQUIRED")
+
+        }
+
+        BiometricManager.BIOMETRIC_ERROR_UNSUPPORTED -> {
+            onError.invoke("BIOMETRIC_ERROR_UNSUPPORTED")
+        }
+
+        BiometricManager.BIOMETRIC_STATUS_UNKNOWN -> {
+            onError.invoke("Something went wrong !!")
+        }
+    }
+}
